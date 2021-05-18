de53c33b30d6d6afe5b866a135de8f32fe3339fb
Hwimli <iimen826@gmail.com>
2021-05-18 12:50:19 +0100

    first commit

--- routes/person.js
+++ routes/person.js
@@ -3,28 +3,7 @@ const express=require('express')
 const router=express.Router()
 const Person=require('../models/Person')
 
-/*Create and Save a Person */
-router.post('/person',(req,res)=>{
-    const {name,age,favoriteFoods}=req.body
-    //Create a document instance using the Person :
-    const person=new Person({
-        name,
-        age,
-        favoriteFoods, 
-    })
-    person.save()
-    .then(person=>res.status(200).json('person add with succes'))
-    .catch(err=>res.status(400).json(err))
 
-
-})
-
-//request Delete
-router.delete('/person/:id',(req,res)=>{
-    Person.findByIdAndDelete(req.params.id)
-    .then(user=>res.status(200).json(user))
-    .catch(err=>res.status(400).json(err))
-})
 
 
     
--- server.js
+++ server.js
@@ -17,18 +17,18 @@ mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true},{connectTimeoutM
     console.log('connect to db...')
 })
 /* Save person */
-const person = new PersonModel({ name: 'Amounnnn' });
+const person = new PersonModel({ name: 'Amounnnn' ,age:25, favoriteFoods: ["Spagetti"]});
 person.save(function (err) {
-  if (err) return handleError(err);
+  if (err) return handleError(err);   
   else console.log("user saved !")
   // saved!
 });
 
 /** Create many People with `Model.create()` */
 var arrayOfPeople = [
-    {name: "anas", age: 74,   favoriteFoods: ["pizza"]},
-    {name: "emy", age: 76,    favoriteFoods: ["roast chicken"]},
-    {name: "rayhan", age: 78, favoriteFoods: ["hamburger"]}
+   {name: "anas", age: 74,   favoriteFoods: ["pizza"]},
+   {name: "emy", age: 76,    favoriteFoods: ["roast chicken"]},
+   {name: "rayhan", age: 78, favoriteFoods: ["hamburger"]}
   ];
  
 
@@ -40,11 +40,79 @@ var arrayOfPeople = [
   )
   //Find persons by given name !
 PersonModel.find({ name: 'anas' }).exec((err,person)=>{
-    err ? console.log(err)  : console.log("liste user",person)
+    err ? console.log(err)  : console.log(" user by this name :",person)
+  });
+
+//Find persons by favoriteFoods !   
+PersonModel.find({ favoriteFoods: 'hamburger' }).exec((err,person)=>{
+  err ? console.log(err)  : console.log("Person has favorit food hamburger is :",person)
+});
+//Find persons by id  !
+
+var id = '60a392fe86ca8444ccaf57cb';
+PersonModel.findById(id, function (err, person) {
+    if (err){
+        console.log(err);
+    }
+    else{
+        console.log("person with this id is  : ", person);
+    }
+});
+// Updates person favoriteFoods : 
+var id = '60a392fe86ca8444ccaf57cb';
+PersonModel.findByIdAndUpdate({ _id: id }, { $push: { favoriteFoods: "hamburgggg" } },{ new: true },(err,person)=>{
+
+    if (err){
+      console.log(err);
+  }
+  else{
+      console.log("favoriteFoods After update  : ", person);
+  }
   });
 
   
+// Updates person age : 
+var name = 'anas';
+PersonModel.update({ name:name }, {$set: { age: 20 }}
+,(err,person)=>{
+
+    if (err){
+      console.log(err);
+  }
+  else{
+      console.log("Person  age after update  : ", person);
+  }
+  });
+  //Delete person by given id : 
+  var id='60a39fc5187f836d4029e123'
+   PersonModel.findOneAndRemove({ _id: id },(err,person)=>{
+    if (err){
+      console.log(err);
+  }
+  else{
+      console.log("Person deleted with success : ", person);  
+    }
+   });
+
+//Delete Many Documents   
+var name='anas'
+PersonModel.deleteMany({name: name },(err,person)=>{
+ if (err){
+   console.log(err);
+}
+else{
+   console.log("Person deleted with success  ", person);  
+ }
+});
+//// Chain Search Query Helpers to Narrow Search Results
+ PersonModel.find({ favoriteFoods:{$in:['roast chicken']}}).sort({name: 1}).limit(2) .select({age: 0}) 
+ .exec((err,person)=>{
+  err ? console.log(err)  : console.log(" Result of Chain Search :",person)
+});           
+
+
+
 app.use('/api',require('./routes/person'))
-app.listen(4006,()=>{
+app.listen(4000,()=>{
     console.log("connected")
 })
